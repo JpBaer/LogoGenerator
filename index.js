a33ebc444f31b4fca4c5afd81420bf30ccb6414e
Jorgen Baertsch <jorgenpaulmusic@gmail.com>
2023-03-10 12:45:42 -0800

    Files created

---
+++ index.js
@@ -0,0 +1,8 @@
+//Logo Generator
+//When prompted a user inputs text, color, and shape to generate a svg file
+//Creare inquirer prompts for text, text color, shape, and shape color
+//create a function to write to file
+//Create a test file to ensure functionality of app
+
+const inquirer = require('inquirer');
+const fs = require('fs');
---
+++ test/validate.test.js
@@ -0,0 +1,8 @@
+const Validate = require('../index')
+
+    describe('Validate',() => {
+        // Write test to ensure a text input was recieved
+        // Write test to ensure a text color was recieved
+        // Write test to ensure a shape was recieved
+        // Write test to ensure a color was recieved
+    })
